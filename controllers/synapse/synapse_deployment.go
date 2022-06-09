3d5b6634ecbef60813d8ae27dbc0b58e75b0e2d4
mgoerens <mgoerens@redhat.com>
2022-06-09 16:41:39 +0200

    Update Synapse, Heisenbridge, and Postgres images
    - Update Synapse to v1.60.0
    - Update Heisenbridge to 1.13
    - Update Postgres instance to ubi8-14.3-0, now using PostgresSQL 14
    - Update crunchy-pgbackrest to ubi8-2.38-1

--- controllers/synapse/synapse_deployment.go
+++ controllers/synapse/synapse_deployment.go
@@ -47,7 +47,7 @@ func (r *SynapseReconciler) deploymentForSynapse(s *synapsev1alpha1.Synapse, obj
 				},
 				Spec: corev1.PodSpec{
 					InitContainers: []corev1.Container{{
-						Image: "matrixdotorg/synapse:v1.46.0",
+						Image: "matrixdotorg/synapse:v1.60.0",
 						Name:  "synapse-generate",
 						Args:  []string{"generate"},
 						Env: []corev1.EnvVar{{
@@ -69,7 +69,7 @@ func (r *SynapseReconciler) deploymentForSynapse(s *synapsev1alpha1.Synapse, obj
 						}},
 					}},
 					Containers: []corev1.Container{{
-						Image: "matrixdotorg/synapse:v1.46.0",
+						Image: "matrixdotorg/synapse:v1.60.0",
 						Name:  "synapse",
 						Env: []corev1.EnvVar{{
 							Name:  "SYNAPSE_CONFIG_PATH",
