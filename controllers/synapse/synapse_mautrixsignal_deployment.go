2948a6c60f0efe41ad51680ed89bbaf97068e0d5
mgoerens <mgoerens@redhat.com>
2022-06-16 17:10:17 +0200

    Add option to deploy the mautrix-signal bridge
    It is composed of:
    * mautrix-signal: Deployement, Service, 1 PVC to host the bridge's data,
    1 PVC to host the SQLite database file, and a ConfigMap to provide the
    input config.yaml
    * signald: Deployment and PVC
    
    As for the configuration of Synapse (homeserver.yaml), and Heisenbridge
    (heisenbridge.yaml), the user can provide its own config.yaml for
    mautrix-signal. In that case, a copy of the user-provided ConfigMap is
    created. In the copied object, some configuration options are
    overwritten. The user-provided ConfigMap is not modified.
    
    Alternatively, if the user doesn't provide a ConfigMap, a default
    config.yaml is used for configuring the mautrix-signal bridge.
    
    The controller also takes care of updating the configuration of Synapse
    so that it is aware of the newly deployed bridge (added as an appservice
    in homeserver.yaml)

---
+++ controllers/synapse/synapse_mautrixsignal_deployment.go
@@ -0,0 +1,119 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package synapse
+
+import (
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	synapsev1alpha1 "github.com/opdev/synapse-operator/apis/synapse/v1alpha1"
+)
+
+// labelsForMautrixSignal returns the labels for selecting the resources
+// belonging to the given synapse CR name.
+func labelsForMautrixSignal(name string) map[string]string {
+	return map[string]string{"app": "mautrix-signal", "synapse_cr": name}
+}
+
+// deploymentForMautrixSignal returns a Deployment object for the mautrix-signal bridge
+func (r *SynapseReconciler) deploymentForMautrixSignal(s *synapsev1alpha1.Synapse, objectMeta metav1.ObjectMeta) (client.Object, error) {
+	ls := labelsForMautrixSignal(s.Name)
+	replicas := int32(1)
+
+	// The created mautrix-signal ConfigMap Name share the same name as the
+	// mautrix-signal Deployment
+	mautrixSignalConfigMapName := objectMeta.Name
+	mautrixSignalPVCName := objectMeta.Name
+	SignaldPVCName := s.ObjectMeta.Name + "-signald"
+
+	dep := &appsv1.Deployment{
+		ObjectMeta: objectMeta,
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: ls,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: ls,
+				},
+				Spec: corev1.PodSpec{
+					// The init container is responsible of copying the
+					// config.yaml from the read-only ConfigMap to the
+					// mautrixsignal-data volume. The mautrixsignal process
+					// needs read & write access to the config.yaml file.
+					InitContainers: []corev1.Container{{
+						Image: "ubi8/ubi-minimal:8.6",
+						Name:  "initconfig",
+						VolumeMounts: []corev1.VolumeMount{{
+							Name:      "config",
+							MountPath: "/input",
+						}, {
+							Name:      "mautrixsignal-data",
+							MountPath: "/data",
+						}},
+						Command: []string{"bin/sh", "-c"},
+						Args:    []string{"if [ ! -f /data/config.yaml ]; then cp /input/config.yaml /data/config.yaml; fi"},
+					}},
+					Containers: []corev1.Container{{
+						Image: "dock.mau.dev/mautrix/signal:v0.3.0",
+						Name:  "mautrix-signal",
+						VolumeMounts: []corev1.VolumeMount{{
+							Name:      "signald",
+							MountPath: "/signald",
+						}, {
+							Name:      "mautrixsignal-data",
+							MountPath: "/data",
+						}},
+					}},
+					Volumes: []corev1.Volume{{
+						Name: "config",
+						VolumeSource: corev1.VolumeSource{
+							ConfigMap: &corev1.ConfigMapVolumeSource{
+								LocalObjectReference: corev1.LocalObjectReference{
+									Name: mautrixSignalConfigMapName,
+								},
+							},
+						},
+					}, {
+						Name: "signald",
+						VolumeSource: corev1.VolumeSource{
+							PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+								ClaimName: SignaldPVCName,
+							},
+						},
+					}, {
+						Name: "mautrixsignal-data",
+						VolumeSource: corev1.VolumeSource{
+							PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+								ClaimName: mautrixSignalPVCName,
+							},
+						},
+					}},
+				},
+			},
+		},
+	}
+	// Set Synapse instance as the owner and controller
+	if err := ctrl.SetControllerReference(s, dep, r.Scheme); err != nil {
+		return &appsv1.Deployment{}, err
+	}
+	return dep, nil
+}
---
+++ controllers/synapse/synapse_signald_deployment.go
@@ -0,0 +1,78 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package synapse
+
+import (
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	synapsev1alpha1 "github.com/opdev/synapse-operator/apis/synapse/v1alpha1"
+)
+
+// labelsForSynapse returns the labels for selecting the resources
+// belonging to the given synapse CR name.
+func labelsForSignald(name string) map[string]string {
+	return map[string]string{"app": "heisenbridge", "synapse_cr": name}
+}
+
+// deploymentForSynapse returns a synapse Deployment object
+func (r *SynapseReconciler) deploymentForSignald(s *synapsev1alpha1.Synapse, objectMeta metav1.ObjectMeta) (client.Object, error) {
+	ls := labelsForSignald(s.Name)
+	replicas := int32(1)
+	signaldPVCName := objectMeta.Name
+
+	dep := &appsv1.Deployment{
+		ObjectMeta: objectMeta,
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: ls,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: ls,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Image: "docker.io/signald/signald:0.19.1",
+						Name:  "signald",
+						VolumeMounts: []corev1.VolumeMount{{
+							Name:      "signald",
+							MountPath: "/signald",
+						}},
+					}},
+					Volumes: []corev1.Volume{{
+						Name: "signald",
+						VolumeSource: corev1.VolumeSource{
+							PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+								ClaimName: signaldPVCName,
+							},
+						},
+					}},
+				},
+			},
+		},
+	}
+	// Set Synapse instance as the owner and controller
+	if err := ctrl.SetControllerReference(s, dep, r.Scheme); err != nil {
+		return &appsv1.Deployment{}, err
+	}
+	return dep, nil
+}
